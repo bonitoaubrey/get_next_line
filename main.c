e7564ecc4a295a1e6587b7aeaf158071106c2b35
DAV <corpseDAV@gmail.com>
2021-06-08 08:59:18 -0400

    new main

--- main.c
+++ main.c
@@ -1,26 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/11 13:39:15 by agcolas           #+#    #+#             */
-/*   Updated: 2021/06/08 08:44:05 by dav              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
-//#include <sys/types.h>
-//#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-
-/*
-** 1 : Une ligne a été lue
-** 0 : La fin de fichier a été atteinte
-** -1 : Une erreur est survenue
-*/
 
 int	main(void)
 {
